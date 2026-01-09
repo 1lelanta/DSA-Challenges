dd27902eff928eac3e209c3ef5ab4117fc62ed0e
!lelanta <kekikoki4@gmail.com>
2026-01-09 18:59:10 +0300

    add pop operation

--- stack.cpp
+++ stack.cpp
@@ -4,7 +4,7 @@ using namespace std;
 static int stack[UPPERLIMIT];
 
 top = -1;
-int addItem(int item){
+void addItem(int item){
     top=top+1;
     if(top <UPPERLIMIT){
         stack[top] = item;
@@ -13,3 +13,18 @@ int addItem(int item){
         return;
     }
 }
+
+void del_el(){
+    int del_val = 0;
+
+    if(top==-1){
+        cout<<"stack underflow";
+    }
+    else{
+        del_val = stack[top];
+        stack[top] = NULL;
+        top-=1;
+    }
+    return;
+}
+

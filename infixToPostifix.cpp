556b9659094c0f5b955c552a024363c720f70d2c
!lelanta <kekikoki4@gmail.com>
2026-01-10 09:25:34 +0300

    fixed bug

--- infixToPostifix.cpp
+++ infixToPostifix.cpp
@@ -1,53 +1,52 @@
 #include<iostream>
 #include<string>
+#include <cctype>
 using namespace std;
 
 int precedence(char op){
-    if(op=='(') return 0;
-    if(op=="+" || op =="-") return 1;
-    if(op=="*"|| op =="/") return 2;
-
-    return -1;
+	if(op == '(') return 0;
+	if(op =='+' || op == '-') return 1;
+	if(op=='*'|| op=='/') return 2;
+	
+	return -1;
 }
 
 string infixToPostifix(string infix){
-    char stack[100];
-    int top = -1;
-    string postfix="";
+	char stack[100];
+	string postifix="";
+	int top=-1;
+	for(int i=0; i<infix.length(); i++){
+	
+	
+	char item = infix[i];
+	if(isalnum(item)){
+		postifix+=item;
+	}
+	else if(item=='('){
+		stack[++top] = item;
+	}
+	else if(item==')'){
+		while(top!=-1&&stack[top]!='('){
+			postifix+=stack[top--];
+		}
+		top--;
+	}
+	else {
+		while(top!=-1&&precedence(stack[top])>=precedence(item)){
+			postifix +=stack[top--];
+		}
+		stack[++top]=item;
+	}
 
-    for(int i=0; i<infix.length(); i++){
-        char item = infix[i];
-        if(isalnum(item)){
-            postfix+=item
-        }
-        else if(item=='('){
-            stack[++top] = item;
-        }
-        else if(item ==')'){
-            while(top!=-1 && stack[top]!='('){
-                postfix += stack[top--];
-            }
-            top--;
-        }
-        else{
-            while (top!=-1 && precedenc(stack[top]> precedence(item))){
-                postfix+= stack[top--]
-            }
-            stack[++top]= item;
-
-            
-        }
-    }
-
-    while (top!=-1){
-        postfix+=stack[top--];
-    }
-
-    return postifix;
+}
+	while(top!=-1){
+		postifix+=stack[top--];
+	}
+	return postifix;
 }
 
-int main() {
-    string infix = "a+b*c+(d*e+f)*g";
-    cout << "Postfix: " << infixToPostfix(infix) << endl;
+int main(){
+	string infix = "a+b*c+(d*e+f)*g";
+    cout << "Postfix: " << infixToPostifix(infix) << endl;
     return 0;
 }
